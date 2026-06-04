d2a4b877462e1a78c37f65f962b14c149464990b
gaby001345-stack <gaby001345@gmail.com>
2026-06-03 20:54:04 -0600

    Actualizar App y LanguageContext: reorganizar proveedores y corregir errores tipográficos en la gestión del idioma

--- MiAppMovil/App.tsx
+++ MiAppMovil/App.tsx
@@ -3,14 +3,21 @@ import StackNavigator from "./src/navigation/StackNavigator";
 import { navigationRef } from "./src/navigation/NavigationService";
 import { AuthProvider } from "./src/context/AuthContext";
 import { LanguageProvider } from "./src/context/LanguageContext";
+import { SkincareProvider } from "./src/context/SkincareContext";
+import { ThemeProvider } from "./src/context/ThemeContext";
+
 export default function App() {
- return (
-    <LanguageProvider>
-    <AuthProvider>
-      <NavigationContainer ref={navigationRef}>
-        <StackNavigator />
-      </NavigationContainer>
-    </AuthProvider>
-    </LanguageProvider>
+  return (
+    <ThemeProvider>
+      <LanguageProvider>
+        <AuthProvider>
+          <SkincareProvider>
+            <NavigationContainer ref={navigationRef}>
+              <StackNavigator />
+            </NavigationContainer>
+          </SkincareProvider>
+        </AuthProvider>
+      </LanguageProvider>
+    </ThemeProvider>
   );
 }
--- MiAppMovil/src/context/LanguageContext.tsx
+++ MiAppMovil/src/context/LanguageContext.tsx
@@ -1,43 +1,39 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { I18n } from "i18n-js";
 import { createContext, useContext, useEffect, useState } from "react";
+import { translations } from "../utils/Translations";
 
-//1 tipado de objeto principal del contexto
-type Language = 'es'|'en';
-
+type Language = 'es' | 'en' |'';
 
 type LanguageContextType = {
     language: Language;
-    changeLanguaje: (lng: Language) => void;
+    changeLanguage: (lng: Language) => void;
+    clearLanguage: () => void;
 }
 
-//definición de diccionarios
-const translations = {
-    en: {signIn: 'Sign in', goToSettings: 'Go to settings', welcome:"Welcome Home!"},
-    es: {signIn: 'Iniciar sesión', goToSettings: 'Ir a User Settings', welcome:"¡Bienvenido a Home!"}
-};
-
-//Crear instancia de i18n con diccionario cargado
+//1. definicion de diccionario para idiomas en /utils
+//2. crear instancia de i18n con diccionario cargado
 const i18n = new I18n(translations);
 
-//Definir propiedades como idioma por defecto, habilitar fallback
-//2. Creacion del contexto
-const LanguageContext = createContext<LanguageContextType | null>(null);
+//3. definir propiedades como idioma por defecto, habilitar fallback 
+i18n.defaultLocale = "en";
+i18n.enableFallback = true;
+
+const LanguageContext = createContext<LanguageContextType| null>(null);
 
 export const useLanguage = () => {
     const context = useContext(LanguageContext);
     if (!context) throw new Error ("useLanguage debe usarse dentro de LanguageProvider");
     return context;
 }
-//3 crear el provider: medio por el cual se maneja el estado global
-export const LanguageProvider = ({children}: {children: React.ReactNode}) => {
-    //inicializacion de estado con valor por defecto
-    const [language, setLanguage] = useState<Language|null>(null);
 
-    useEffect(() => {
-        const loadLanguage = async () => {
-            const storedLanguage = await AsyncStorage.getItem('language');
-            if (storedLanguage) {
+export const LanguageProvider = ({children}: {children: React.ReactNode}) => {
+    const [language, setLanguage] = useState<Language>("");
+
+    useEffect(()=>{
+        const loadLanguage = async () =>{
+            const storedLanguage = await AsyncStorage.getItem("language");
+            if (storedLanguage){
                 setLanguage(storedLanguage as Language);
                 i18n.locale = storedLanguage;
             }else {
@@ -45,19 +41,22 @@ export const LanguageProvider = ({children}: {children: React.ReactNode}) => {
             }
         };
         loadLanguage();
-    }, [language]);
-    
-    const changeLanguaje =  async (lng: Language) => {
-    setLanguage(lng);
-    i18n.locale = lng;
-    await AsyncStorage.setItem('language', lng);
-  };
-  
-  return(
-    <LanguageContext.Provider value={{language, changeLanguaje}}>
-        {children}
-    </LanguageContext.Provider>
- );
+    }, []);
+
+    const changeLanguage = async (lng: Language) => {
+        setLanguage(lng);
+        i18n.locale = lng; 
+        await AsyncStorage.setItem("language", lng);
+    };
+
+    const clearLanguage = async () => {
+        await AsyncStorage.removeItem("language")
+    };
+    return(
+        <LanguageContext.Provider value={{language, changeLanguage, clearLanguage}}>
+            {children}
+        </LanguageContext.Provider>
+    );
 }
 
 export {i18n};
